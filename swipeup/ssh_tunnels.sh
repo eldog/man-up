fac51b9901c7ec4a63ab83a3d72852efbe1a2696
Peter Sutton <petersutton2009@gmail.com>
2011-08-12 23:31:09 +0100

    Remoted unneed ssh port forward.

--- swipeup/ssh_tunnels.sh
+++ swipeup/ssh_tunnels.sh
@@ -19,8 +19,7 @@ done
 if [[ -z "${KILL}" ]]; then
     echo "Starting SSH tunnels to $CS_USERNAME"
     ssh -N -L 8000:edir.man.ac.uk:389 \
-           -L 8001:ldap.man.ac.uk:389 \
-           "$CS_USERNAME@$CS_MACHINE.cs.man.ac.uk" &
+        "$CS_USERNAME@$CS_MACHINE.cs.man.ac.uk" &
     echo $! > "${SSH_PID}"
 else
     echo "Stopping SSH tunnels."
