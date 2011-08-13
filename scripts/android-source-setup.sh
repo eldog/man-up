27868fe8871a8cba7577435d638d48034f502576
Lloyd Henning <lloyd.w.henning@gmail.com>
2011-08-13 11:59:31 +0100

    Now installing everything to /opt. Added android sdk install

--- scripts/android-source-setup.sh
+++ scripts/android-source-setup.sh
@@ -3,21 +3,31 @@ set -o errexit
 set -o nounset
 
 REPO_MD5='bbf05a064c4d184550d71595a662e098'
-REPO_PATH="$HOME/Dropbox/bin/repo"
+REPO_DIR='/opt/bin'
+REPO_NAME='repo'
+REPO_PATH="${REPO_DIR}/${REPO_NAME}"
 REPO_URL='https://android.git.kernel.org/repo'
 
-SOURCE_PATH="$HOME/android-source"
+SOURCE_DIR="android-source"
+SOURCE_TMP_DIR="/tmp/${SOURCE_DIR}"
 
 # Setup repo
-wget -O "$REPO_PATH" "$REPO_URL"
+if [ ! -d "${REPO_DIR}" ]; then
+    sudo mkdir "${REPO_DIR}"
+fi    
+sudo wget -O "$REPO_PATH" "$REPO_URL"
 if [ "`openssl md5 "$REPO_PATH" | cut -d ' ' -f 2`" != "$REPO_MD5" ]; then
     echo 'Repo invalid! Aborting.'
     exit 1
 fi
-chmod a+x "$REPO_PATH"
+sudo chmod a+x "$REPO_PATH"
 
 # Download source
-mkdir "$SOURCE_PATH"
-cd "$SOURCE_PATH"
-repo init -u git://android.git.kernel.org/platform/manifest.git
-repo sync
+mkdir "${SOURCE_TMP_DIR}"
+cd "${SOURCE_TMP_DIR}"
+"${REPO_PATH}" init -u git://android.git.kernel.org/platform/manifest.git
+"${REPO_PATH}" sync
+
+# Move to source to opt
+cd ..
+sudo mv "${SOURCE_DIR}" /opt
