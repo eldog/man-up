b659172f30e5810a815597cf38ae2f48b5f5ee5e
Peter Sutton <petersutton2009@gmail.com>
2011-08-30 12:48:58 +0100

    Updated appengine script to get latest SDK.

--- scripts/appengine-sdk-setup.sh
+++ scripts/appengine-sdk-setup.sh
@@ -4,14 +4,14 @@ set -o nounset
 
 SDK_DIR='google_appengine'
 SDK_URL='http://googleappengine.googlecode.com/files'
-SDK_URL="$SDK_URL/google_appengine_1.5.2.zip"
-SDK_ZIP='google_appengine_1.5.2.zip'
+SDK_URL="${SDK_URL}/google_appengine_1.5.3.zip"
+SDK_ZIP='google_appengine_1.5.3.zip'
 
-cd /tmp
+cd /tmp/
 wget "${SDK_URL}"
-unzip "${SDK_ZIP}"
-rm -fr "${SDK_ZIP}"
-cd "${SDK_DIR}"
+unzip "./${SDK_ZIP}"
+rm -fr "./${SDK_ZIP}"
+cd "./${SDK_DIR}"
 
 # Change hash bang to '#!/usr/bin/env python2.5'
 sed -i '1s/$/2.5/' dev_appserver.py
@@ -19,8 +19,10 @@ sed -i '1s/$/2.5/' dev_appserver.py
 # Enable TLS in mail
 sed -i '192i\      smtp.ehlo()\
       smtp.starttls()\
-      smtp.ehlo()' 'google/appengine/api/mail_stub.py'
+      smtp.ehlo()' google/appengine/api/mail_stub.py
 
 cd ..
-sudo chown -R root:root "${SDK_DIR}"
-sudo mv "${SDK_DIR}" /opt/
+sudo chown -R root:root "./${SDK_DIR}"
+# If exists, remove currently installed SDK.
+sudo rm -fr "/opt/${SDK_DIR}"
+sudo mv "./${SDK_DIR}" /opt/
