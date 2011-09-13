63e00b83f39b9afbbc94fdd275098c1b70b623c3
Peter Sutton <petersutton2009@gmail.com>
2011-09-13 15:17:35 +0100

    Moved everything into one file.

--- text-to-spit/src/swift.py
+++ text-to-spit/src/swift.py
@@ -1,14 +1,37 @@
 from argparse import ArgumentParser
 from collections import namedtuple
+from xml.dom.minidom import getDOMImplementation
 import queue
 import subprocess
 import sys
 import threading
 
-from ssml import SsmlDocument
-
 SWIFT_NAME = 'swift'
 WHICH_PATH = '/bin/which'
+
+_imp = getDOMImplementation()
+
+class SsmlDocument:
+    def __init__(self):
+        self._doc = _imp.createDocument('http://www.w3.org/2001/10/synthesis',
+            'speak', None)
+        self.toxml = self._doc.toxml
+        self.toprettyxml = self._doc.toprettyxml
+        self._speak = self._doc.getElementsByTagName('speak')[0]
+        self._speak.setAttribute('version', '1.0')
+        self._speak.setAttribute('xml:lang', 'en-US')
+        self._speak.setAttribute('xmlns', 'http://www.w3.org/2001/10/synthesis')
+        self._speak.setAttribute('xsi:schemaLocation',
+            'http://www.w3.org/2001/10/synthesis '
+            'http://www.w3.org/TR/speech-synthesis/synthesis.xsd')
+        self._speak.setAttribute('xmlns:xsi',
+            'http://www.w3.org/2001/XMLSchema-instance')
+
+    def append_sentance(self, sentence):
+        element = self._doc.createElement('s')
+        self._speak.appendChild(element)
+        text = self._doc.createTextNode(sentence)
+        element.appendChild(text)
 
 SmsMessage = namedtuple('SmsMessage', ('number', 'message'))
 
@@ -130,7 +153,6 @@ def main(argv=None):
     sms_supplier.mainloop();
 
     sms_handler.join()
-
     print('Exiting.')
 
 if __name__ == '__main__':
