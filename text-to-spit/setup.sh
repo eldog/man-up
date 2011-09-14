dfe7a2656c4183ac235ee1c7d2f4239591d20160
Lloyd Henning <lloyd.w.henning@gmail.com>
2011-09-14 11:26:08 +0100

    updated setup to install sox anf ffmpeg

--- text-to-spit/setup.sh
+++ text-to-spit/setup.sh
@@ -6,7 +6,9 @@ CHERRYPY_URL='http://download.cherrypy.org/cherrypy'
 CHERRYPY_URL="${CHERRYPY_URL}/3.2.0/CherryPy-3.2.0-py3.1.egg"
 
 sudo apt-get --assume-yes install \
-    python3-setuptools
+    ffmpeg \
+    python3-setuptools \
+    sox
 
 sudo easy_install3 "${CHERRYPY_URL}"
 
