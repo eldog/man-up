634a2a74d87d562b27e53e66a9da6d4c8e128d88
Peter Sutton <petersutton2009@gmail.com>
2011-09-14 21:53:28 +0100

    Added commands that make this easy to demo.

---
+++ text-to-spit/commands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+HOSTNAME="`hostname -I`"
+
+echo "Hostname: ${HOSTNAME}"
+
+function docurl
+{
+    local number="${1}"; shift;
+    curl -s \
+        -d number="${number}" \
+        -d message="${*}" \
+        -o /dev/null \
+        "${HOSTNAME}:8080"
+
+}
+
+alias user='docurl +447555555555'
+alias admin='docurl admin'
+alias admin-speech='admin speech'
+alias admin-compile='admin compile'
+alias admin-rap='admin rap'
+alias run="python3 tts.py --hostname ${HOSTNAME}"
+
