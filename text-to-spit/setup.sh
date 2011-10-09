b7b40ce6dcef36ee59815a49d01da2bcbe9bd25c
Peter Sutton <petersutton2009@gmail.com>
2011-10-09 20:09:43 +0100

    Updated setup script.

--- text-to-spit/setup.sh
+++ text-to-spit/setup.sh
@@ -6,6 +6,7 @@ CHERRYPY_URL='http://download.cherrypy.org/cherrypy'
 CHERRYPY_URL="${CHERRYPY_URL}/3.2.0/CherryPy-3.2.0-py3.1.egg"
 
 sudo apt-get --assume-yes install \
+    curl \
     ffmpeg \
     python3-setuptools \
     sox
