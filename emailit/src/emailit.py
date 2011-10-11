1b4842ddfe5f2f1e05c3a6e1707bc6aaa7b6918a
Peter Sutton <petersutton2009@gmail.com>
2011-10-11 22:37:00 +0100

    emailit can read emails directly from a file.

--- emailit/src/emailit.py
+++ emailit/src/emailit.py
@@ -16,7 +16,7 @@ with open(resolve_path(__file__, 'template.html')) as template_file:
     TEMPLATE = template_file.read()
 
 TEMPLATE_FACEBOOK_EVENT = '''
- | <a href="http://www.facebook.com/event.php?eid={event_id}" target="_blank" style="color: #336699; font-weight: normal; text-decoration: underline;">Facebook Event</a>
+ | <a href="http://www.facebook.com/event.php?eid={event_id}" target="_blank" style="color: #336699; font-weight: normal; text-decoration: underline;">Facebook event</a>
 '''
 
 TEMPLATE_IMAGE = '''
@@ -35,69 +35,78 @@ TEMPLATE_ONLINE_VERSION = '''
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-    ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser(fromfile_prefix_chars='@')
     ap.add_argument('-b', '--body', required=True)
     ap.add_argument('-c', '--computer', default='eng007')
+    ap.add_argument('-d', '--dry-run', action='store_true')
     ap.add_argument('-f', '--facebook-event', type=int)
     ap.add_argument('-i', '--image-name')
     ap.add_argument('-l', '--image-link')
     ap.add_argument('-o', '--online-index', type=int)
     ap.add_argument('-s', '--subject', required=True)
-    ap.add_argument('-t', '--to', required=True)
+    ap.add_argument('-t', '--to', nargs='+', required=True)
     ap.add_argument('-u', '--username', required=True)
     args = ap.parse_args(args=argv[1:])
 
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = args.subject
-    msg['From'] = '%s@cs.man.ac.uk' % args.username
-    msg['To'] = args.to
+    for to in args.to:
+        print('%s...' % to, end='')
+
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = args.subject
+        msg['From'] = '%s@cs.man.ac.uk' % args.username
+        msg['To'] = to
 
 
-    with open(args.body) as body_file:
-        body_plain = body_file.read()
+        with open(args.body) as body_file:
+            body_plain = body_file.read()
 
-    msg.attach(MIMEText(body_plain))
+        msg.attach(MIMEText(body_plain))
 
-    if args.facebook_event:
-        facebook_event = TEMPLATE_FACEBOOK_EVENT.format(
-            event_id=args.facebook_event)
-    else:
-        facebook_event = ''
+        if args.facebook_event:
+            facebook_event = TEMPLATE_FACEBOOK_EVENT.format(
+                event_id=args.facebook_event)
+        else:
+            facebook_event = ''
 
-    if args.image_name:
-        image = TEMPLATE_IMAGE.format(image_name=args.image_name)
-        if args.image_link:
-            image = '<a href="%s" target="_blank">%s</a>' % (args.image_link,
-                image)
-    else:
-        image = ''
+        if args.image_name:
+            image = TEMPLATE_IMAGE.format(image_name=args.image_name)
+            if args.image_link:
+                image = '<a href="%s" target="_blank">%s</a>' \
+                    % (args.image_link, image)
+        else:
+            image = ''
 
 
-    if args.online_index is not None:
-        online_version = TEMPLATE_ONLINE_VERSION.format(
-            index=args.online_index)
-    else:
-        online_version = ''
+        if args.online_index is not None:
+            online_version = TEMPLATE_ONLINE_VERSION.format(
+                index=args.online_index)
+        else:
+            online_version = ''
 
-    html = TEMPLATE.format(
-        body=markdown.markdown(body_plain),
-        image=image,
-        facebook_event=facebook_event,
-        online_version=online_version)
+        html = TEMPLATE.format(
+            body=markdown.markdown(body_plain),
+            image=image,
+            facebook_event=facebook_event,
+            online_version=online_version)
 
-    msg.attach(MIMEText(html, 'html'))
+        msg.attach(MIMEText(html, 'html'))
 
-    with open('/tmp/msg', 'w') as msg_file:
-        msg_file.write(msg.as_string())
+        with open('/tmp/msg', 'w') as msg_file:
+            msg_file.write(msg.as_string())
 
-    subprocess.check_call('scp /tmp/msg %s@%s.cs.man.ac.uk:~/msg'
-        % (args.username, args.computer), shell=True,
-        stdout=open(os.devnull, 'w'))
+        subprocess.check_call('scp /tmp/msg %s@%s.cs.man.ac.uk:~/msg'
+            % (args.username, args.computer), shell=True,
+            stdout=open(os.devnull, 'w'))
 
-    subprocess.check_call(
-        "ssh %s@%s.cs.man.ac.uk '/usr/sbin/sendmail -v %s < ~/msg'"
-        % (args.username, args.computer, args.to), shell=True,
-        stdout=open(os.devnull, 'w'))
+        if not args.dry_run:
+            subprocess.check_call(
+                "ssh %s@%s.cs.man.ac.uk '/usr/sbin/sendmail -v %s < ~/msg'"
+                % (args.username, args.computer,
+                    to), shell=True,
+                stdout=open(os.devnull, 'w'))
+            print('done')
+        else:
+            print('skipped')
 
     return 0
 
