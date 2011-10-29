febcf26279df4302293c7d3fe90a01238646423f
Lloyd <lloyd.w.henning@gmail.com>
2011-10-29 18:10:16 +0100

    added jqueary async

--- notld/lift/button_server.py
+++ notld/lift/button_server.py
@@ -1,3 +1,4 @@
+from multiprocessing import Pipe
 from pprint import pprint
 from twisted.web.server import Site
 from twisted.web.resource import Resource
@@ -7,16 +8,23 @@ from twisted.internet import reactor
 class LiftResource(Resource):
     isLeaf =True
     
-    def render_POST(self, request):
-        pprint(request.__dict__)
-        return 'thanks'
+    def __init__(self, pipe):
+        self.pipe = pipe
 
-def main():
+    def render_POST(self, request):
+        self.pipe.send('button pressed')
+        print 'thanks'
+
+def main(pipe):
     root = File('static')
-    root.putChild('lift', LiftResource())
+    root.putChild('lift', LiftResource(pipe))
     reactor.listenTCP(8080, Site(root))
     reactor.run()
 
+class DummyPipe(object):
+    def send(*args, **kwargs):
+        pass
+
 print 'rofl'
 if __name__ == '__main__':
-    main()
+    main(DummyPipe())
